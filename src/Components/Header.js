d9c76058e18463eb213f389d967e2402040f78a5
Ronnie Song <ronniesong0809@gmail.com>
2020-06-15 14:54:52 -0700

    feat; change logo, favicon, and more

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 import {Navbar, Nav} from 'react-bootstrap'
-import { ReactComponent as Logo } from './Logo.svg';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faCode} from '@fortawesome/free-solid-svg-icons'
 
 class Headers extends Component {
   render() {
     return (
       <Navbar sticky="top" bg="dark" variant="dark" expand="lg">
         <Navbar.Brand href="/">
-          <Logo fill="white" src="/Logo.svg" width="30" height="30" className="d-inline-block align-top" />{' '}
+          <FontAwesomeIcon icon={faCode}/>{' '}
           Hacker News Top 100
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
